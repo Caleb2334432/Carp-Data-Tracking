21166ab71fb4d95f8259c925fbb1098402f44396
Caleb2334432 <105329729+Caleb2334432@users.noreply.github.com>
2025-04-18 18:57:24 -0500

    Update MapOverlay.jsx

--- src/components/MapOverlay.jsx
+++ src/components/MapOverlay.jsx
@@ -18,13 +18,10 @@ const regions = {
     style: "absolute left-[273px] top-[129px] w-[32px] h-[125px] rotate-[62deg]",
   },
   R5: {
-  {
   color: "bg-purple-500/60",
   style: "absolute left-[284px] top-[112px] w-[138px] h-[42px] rotate-[-106deg] border-tl-[43px] border-tr-[43px] border-bl-[0px] border-br-[0px] z-[9999] flex justify-center items-center text-white font-bold text-center",
   text: "Region 5"
-}
 
-}
 
 }
 
